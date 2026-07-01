f8b7fbaf0071b4f0287fd365ae2d7417fd0ff4a6
gmirtskhulava <mgiorgi685@gmail.com>
2026-07-01 19:26:44 +0400

    Moved Developer-page controllers to controller folder

--- src/main/java/org/example/finalproject/controllers/DeveloperAppController.java
+++ src/main/java/org/example/finalproject/controllers/DeveloperAppController.java
@@ -1,4 +1,4 @@
-package org.example.finalproject;
+package org.example.finalproject.controllers;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -8,6 +8,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import org.example.finalproject.MySQL;
+import org.example.finalproject.User;
+import org.example.finalproject.Utils;
 
 import java.io.IOException;
 import java.net.URL;
--- src/main/java/org/example/finalproject/controllers/DevelopersController.java
+++ src/main/java/org/example/finalproject/controllers/DevelopersController.java
@@ -1,4 +1,4 @@
-package org.example.finalproject;
+package org.example.finalproject.controllers;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -8,6 +8,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import org.example.finalproject.MySQL;
+import org.example.finalproject.User;
+import org.example.finalproject.Utils;
 
 import java.io.IOException;
 import java.net.URL;
